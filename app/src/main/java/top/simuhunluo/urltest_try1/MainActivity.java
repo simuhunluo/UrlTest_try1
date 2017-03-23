7a3f19b782cd38e0522c8f5c35e704e4c2d7b415
scc <simuhunluo@outlook.com>
2017-03-23 12:47:24 +0800

    3-23

--- app/src/main/java/top/simuhunluo/urltest_try1/MainActivity.java
+++ app/src/main/java/top/simuhunluo/urltest_try1/MainActivity.java
@@ -3,11 +3,21 @@ package top.simuhunluo.urltest_try1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            URL url=new URL("");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
